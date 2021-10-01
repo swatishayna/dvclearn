5d2aa5c178381e527b4e862c8ee713b0295a167f
Swati <swati308mmu@gmail.com>
2021-10-01 17:33:06 +0530

    artifacts directory created and data loaded

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -6,3 +6,13 @@ def read_yaml(path_to_yaml:str)->dict:
     with open(path_to_yaml) as yaml_file:
         content =yaml.safe_load(yaml_file)
     return content
+
+
+
+
+
+
+def create_directory(dirs:list):
+    for dir_path in dirs:
+        os.makedirs(dir_path, exist_ok =True)
+        print(f"directory is created at {dir_path}")

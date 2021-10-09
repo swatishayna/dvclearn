55888038c4808029ef376b1bd0cc6d7f4bd1ff20
Swati <swati308mmu@gmail.com>
2021-10-09 07:44:15 +0530

    training stage added

--- src/stage_03_train.py
+++ src/stage_03_train.py
@@ -2,8 +2,8 @@ from src.utils.all_utils import read_yaml, create_directory, save_local_df
 import argparse
 import pandas as pd
 import os
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
+import joblib
 
 def train(config_path, params_path):
     config = read_yaml(config_path)
@@ -29,6 +29,19 @@ def train(config_path, params_path):
     lr = ElasticNet(alpha = alpha, l1_ratio=l1_ratio, random_state=random_state)
     lr.fit(train_x,train_y)
     print("###############################")
+
+
+
+
+    model_dir = config['artifacts']['model_dir']
+    model_filename = config['artifacts']['model_file']
+    model_dir = os.path.join(artifacts_dir,model_dir)
+    create_directory([model_dir])
+    model_path = os.path.join(model_dir, model_filename)
+    joblib.dump(lr, model_path)
+
+
+
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
